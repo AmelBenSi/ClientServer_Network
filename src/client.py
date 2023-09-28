ce87d88996a04c9ae5829e90cc4e1eec3b9838f0
Amel Ben Sidhom <amelsidhom@gmail.com>
2023-09-29 00:41:05 +0200

    I added a section of code that send/receive dictionary

--- src/client.py
+++ src/client.py
@@ -4,17 +4,23 @@ import json
 from dict2xml import dict2xml
 from cryptography.fernet import Fernet
 
-port = 12345
-host = socket.gethostname()
-
-dictionary = {}
-text_file = ()
+PORT = 5050
+HOST = socket.gethostbyname(socket.gethostname())
+SERVER_ADDR = (HOST, PORT)
+FORMAT = "utf-8"
 
 
 # This function should set a dictionary to binary, JSON or XML format, and
 # send it to the server
-def send_dictionary():
-    pass
+def send_dictionary(socket_conn, dictionary, data_format):
+    if data_format == "binary":
+        serialized_dict = pickle.dumps(dictionary)
+    elif data_format == "JSON":
+        serialized_dict = json.dumps(dictionary).encode(FORMAT)
+    elif data_format == "XML":
+        serialized_dict = dict2xml(dictionary)
+
+    socket_conn.send(serialized_dict)
 
 
 # This function should provide the option to encrypt a file, and
@@ -25,7 +31,14 @@ def send_file():
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
+    client_socket.connect(SERVER_ADDR)
+
+    dictionary = {
+        "month": "February",
+        "year": 2020,
+        "day": 21}
+
+    send_dictionary(client_socket, dictionary, "JSON")
 
     client_socket.close()
 
--- src/server.py
+++ src/server.py
@@ -1,30 +1,70 @@
 import socket
+import threading
 import pickle
 import json
-from dict2xml import dict2xml
+import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 
-port = 12345
-host = socket.gethostname()
+
+PORT = 5050
+HOST = ""
+SERVER_ADDR = (HOST, PORT)
+FORMAT = "utf-8"
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDR)
 
 
-# This function should handle the files sent from the client
-def handle_client(client_socket):
-    files = client_socket.recv()
-    pass
+# comment later
+def receive_date(received_data):
+
+    # Try to decode as JSON
+    try:
+        decoded_data = json.loads(received_data.decode())
+        return decoded_data
+    except (json.JSONDecodeError, UnicodeDecodeError):
+        pass
+
+    # Try to decode as binary
+    try:
+        decoded_data = pickle.loads(received_data)
+        return decoded_data
+    except pickle.UnpicklingError:
+        pass
+
+    # Try to decode as XML
+    try:
+        root = ET.fromstring(received_data.decode())
+        decoded_data = {child.tag: child.text for child in root}
+        return decoded_data
+    except ET.ParseError:
+        raise ValueError("Received data is neither valid JSON, binary, nor XML.")
 
 
-# This function establishes the connection with the client
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen()
+# This function should handle all the communications between the client and the server
+def handle_client(client_socket, client_addr):
+    print(f"[NEW CONNECTION] {client_addr} is connected to the server")
+
+    connected = True
+
+    while connected:
+
+        received_data = client_socket.recv(4096)
+        dictionary = receive_date(received_data)
+        print(dictionary)
+        connected = False
+
+
+# This function should handle the connection with the client
+def start():
+    server_socket.listen(5)
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"{client_address} is connected to the server")
-        handle_client(client_socket)
+        client_socket, client_addr = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_addr))
+        thread.start()
 
 
 if __name__ == "__main__":
-    main()
+    print("[STARTING] server is starting...")
+    start()
