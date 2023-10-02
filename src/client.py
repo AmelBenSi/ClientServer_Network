7f04ba4f8768b3f50c4181c2a006a296ca7e936b
Amel Ben Sidhom <amelsidhom@gmail.com>
2023-10-02 17:25:38 +0200

    I updated the section of code in both server/client.py files that handle the exchange of dictionary in JSON, binary and XML formats

--- src/client.py
+++ src/client.py
@@ -2,47 +2,81 @@ import socket
 import pickle
 import json
 from dict2xml import dict2xml
+#import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 
-PORT = 5050
+PORT = 8080
 HOST = socket.gethostbyname(socket.gethostname())
 SERVER_ADDR = (HOST, PORT)
-FORMAT = "utf-8"
 
 
 # This function should set a dictionary to binary, JSON or XML format, and
 # send it to the server
-def send_dictionary(socket_conn, dictionary, data_format):
-    if data_format == "binary":
+def send_dictionary(socket, dictionary, data_format):
+    if data_format == "BINARY":
         serialized_dict = pickle.dumps(dictionary)
     elif data_format == "JSON":
-        serialized_dict = json.dumps(dictionary).encode(FORMAT)
+        serialized_dict = json.dumps(dictionary)
     elif data_format == "XML":
-        serialized_dict = dict2xml(dictionary)
+        serialized_dict = dict2xml(dictionary).decode()
 
-    socket_conn.send(serialized_dict)
+    socket.send(f"SEND_DICTIONARY|{data_format}|{serialized_dict}".encode())
 
 
+        #root = ET.Element("root")
+        #for key, value in dictionary.items():
+        #    ET.SubElement(root, key).text = value
+        #serialized_dict = ET.tostring(root).decode()
+        #"""
+
+
+
+"""
 # This function should provide the option to encrypt a file, and
 # send it to the server
-def send_file():
-    pass
+def send_file(socket, file, encrypt=False):
+    # opening a file to encrypt
+    with open(file, "rb") as file:
+        original_file = file.read()
+
+        if encrypt:
+            # key generation
+            key = Fernet.generate_key()
+
+            # using the generated key
+            fernet = Fernet(key)
+
+            # encrypting the file
+            encrypted_file = fernet.encrypt(original_file)
+
+    socket.send(f"SEND_FILE:{file}|{encrypt}|{encrypted_data.decode()}".encode())
+"""
 
 
-def main():
+def connect():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(SERVER_ADDR)
 
+    # Create a dictionary
     dictionary = {
-        "month": "February",
-        "year": 2020,
-        "day": 21}
+        "year": 2023,
+        "month": "September",
+        "day": 28
+    }
 
-    send_dictionary(client_socket, dictionary, "JSON")
+    # Call send_dictionary function to send the dictionary to the server
+    send_dictionary(client_socket, dictionary, "BINARY")
 
     client_socket.close()
-
+"""
+    # Call send_file function to send a file to the server
+    send_file(client_socket, "file_example.txt", encrypt=True)
+"""
 
 if __name__ == "__main__":
-    main()
+    connect()
+
+
+
+
 
--- src/server.py
+++ src/server.py
@@ -6,58 +6,61 @@ import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 
 
-PORT = 5050
-HOST = ""
+PORT = 8080
+HOST = socket.gethostbyname(socket.gethostname())
 SERVER_ADDR = (HOST, PORT)
-FORMAT = "utf-8"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(SERVER_ADDR)
 
 
-# comment later
-def receive_date(received_data):
+def handle_dict(received_data):
 
-    # Try to decode as JSON
-    try:
-        decoded_data = json.loads(received_data.decode())
-        return decoded_data
-    except (json.JSONDecodeError, UnicodeDecodeError):
-        pass
+    input_data = received_data.decode()
+    print(input_data)
+    first_split = input_data.split("|")
+    print(first_split)
+    header = first_split[0]
+    data_format = first_split[1]
+    serialized_data = first_split[2]
+    print(first_split)
+    print(header)
+    print(data_format)
+    print(serialized_data)
 
-    # Try to decode as binary
-    try:
-        decoded_data = pickle.loads(received_data)
-        return decoded_data
-    except pickle.UnpicklingError:
-        pass
+    if header == "SEND_DICTIONARY":
 
-    # Try to decode as XML
-    try:
-        root = ET.fromstring(received_data.decode())
-        decoded_data = {child.tag: child.text for child in root}
-        return decoded_data
-    except ET.ParseError:
-        raise ValueError("Received data is neither valid JSON, binary, nor XML.")
+        if data_format == "BINARY":
+            original_data = pickle.loads(serialized_data).decode()
+
+        elif data_format == "JSON":
+            original_data = json.loads(serialized_data)
+
+        elif data_format == "XML":
+            root = ET.fromstring(serialized_data)
+            original_data = {child.tag: child.text for child in root}
+
+        else:
+            raise ValueError("Received data is neither valid JSON, binary, nor XML.")
+
+        print(f"[PRINT_TO_SCREEN] Dictionary sent in format {data_format}: {original_data}")
 
 
 # This function should handle all the communications between the client and the server
 def handle_client(client_socket, client_addr):
+
     print(f"[NEW CONNECTION] {client_addr} is connected to the server")
 
     connected = True
-
     while connected:
-
-        received_data = client_socket.recv(4096)
-        dictionary = receive_date(received_data)
-        print(dictionary)
+        received_data = client_socket.recv(1024)
+        handle_dict(received_data)
         connected = False
 
 
 # This function should handle the connection with the client
 def start():
-    server_socket.listen(5)
+    server_socket.listen()
 
     while True:
         client_socket, client_addr = server_socket.accept()
@@ -68,3 +71,7 @@ def start():
 if __name__ == "__main__":
     print("[STARTING] server is starting...")
     start()
+
+
+
+
