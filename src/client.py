9c1c138dae5114f579b3c0e8bab05f8236d369d4
Amel Ben Sidhom <amelsidhom@gmail.com>
2023-10-06 12:33:38 +0200

    added documentation and adjusted certain details to PEP 8

--- src/client.py
+++ src/client.py
@@ -12,67 +12,96 @@ PORT = 8080
 HOST = "localhost"
 SERVER_ADDR = (HOST, PORT)
 
-# Provide your own key. It has to be 16 bites.
+# Provide a 16-byte long key.
 KEY = b"YourOwnSecretKey"
-# Provide a nonce. It has to be 16 bites.
+# Provide a 16-byte long key nonce
 NONCE = b"YourOwnSecretNce"
 
 
-# This function should set a dictionary to binary, JSON or XML format, and
-# send it to the server
-def send_dictionary(socket, dictionary, data_format):
-    serialized_dict = ""
-    dict_size = sys.getsizeof(dictionary)
-    socket.send(str(dict_size).encode())
+# This function serializes a dictionary in BINARY, JSON or XML format, then
+# sends it to the server
+def send_dictionary(conn, dict, data_format):
+    # Get the dictionary´s size
+    dict_size = sys.getsizeof(dict)
+    # Send the dictionary´s size to the server
+    conn.send(str(dict_size).encode())
 
     # B for BINARY
     if data_format == "B":
-        serialized_dict = pickle.dumps(dictionary)
-        socket.send(f"SEND_D|{data_format}".encode())
-        socket.sendall(serialized_dict)
+        # Serialize the dictionary in BINARY format
+        serialized_dict = pickle.dumps(dict)
+        # Send information to the server: Header "SEND_D" and Data Format
+        conn.send(f"SEND_D|{data_format}".encode())
+        # Send the serialized dictionary to the server
+        conn.sendall(serialized_dict)
 
     # J for JSON
     elif data_format == "J":
-        serialized_dict = json.dumps(dictionary)
-        socket.send(f"SEND_D|{data_format}".encode())
-        socket.sendall(serialized_dict.encode())
+        # Serialize the dictionary in JSON format
+        serialized_dict = json.dumps(dict)
+        # Send information to the server: Header "SEND_D" and Data Format
+        conn.send(f"SEND_D|{data_format}".encode())
+        # Send the serialized dictionary to the server
+        conn.sendall(serialized_dict.encode())
 
     # X for XML
     elif data_format == "X":
-        serialized_dict = dict2xml(dictionary, wrap='root', indent="   ")
-        socket.send(f"SEND_D|{data_format}".encode())
-        socket.sendall(serialized_dict.encode())
+        # Serialize the dictionary in XML format
+        serialized_dict = dict2xml(dict, wrap='root', indent="   ")
+        # Send information to the server: Header "SEND_D" and Data Format
+        conn.send(f"SEND_D|{data_format}".encode())
+        # Send the serialized dictionary to the server
+        conn.sendall(serialized_dict.encode())
+
+    else:
+        # Raise an Error if the user types an incorrect value
+        raise ValueError("Only ´B´, ´J´ and ´X´ allowed!")
 
 
-# This function should provide the option to encrypt a file, and
-# send it to the server
-def send_file(socket, file, encrypt):
-
+# This function allows to choose whether to encrypt or not a file, then
+# sends the file to the server
+def send_file(conn, file, encrypt):
     # Opening a file
     with open(file, "rb") as f:
         data = f.read()
 
         # T for TRUE
         if encrypt == "T":
+            # Initialize and AES encryption object
             cipher = AES.new(KEY, AES.MODE_EAX, NONCE)
 
             # Encrypting the file
             encrypted_data = cipher.encrypt(data)
+            # Get the file´s size
             file_size = os.path.getsize(file)
-            socket.send(str(file_size).encode())
-            socket.send(f"SEND_F|{encrypt}".encode())
-            socket.send(encrypted_data)
+            # Send the file´s size to the server
+            conn.send(str(file_size).encode())
+            # Send information to the server: Header "SEND_F" and encryption status
+            conn.send(f"SEND_F|{encrypt}".encode())
+            # Send the encrypted file to the server
+            conn.send(encrypted_data)
 
         # F for FALSE
         elif encrypt == "F":
+            # Get the file´s size
             file_size = os.path.getsize(file)
-            socket.send(str(file_size).encode())
-            socket.send(f"SEND_F|{encrypt}".encode())
-            socket.send(data)
+            # Send the file´s size to the server
+            conn.send(str(file_size).encode())
+            # Send information to the server: Header "SEND_F" and encryption status
+            conn.send(f"SEND_F|{encrypt}".encode())
+            # Send the file to the server
+            conn.send(data)
+
+        else:
+            # Raise an Error if the user types an incorrect value
+            raise ValueError("Only ´T´ and ´F´ allowed!")
 
 
 def connect():
+
+    # create an INET, STREAMing socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # now connect to the web server on port 8080
     client_socket.connect(SERVER_ADDR)
 
     command = input("[SENT_ITEM] Are you sending a DICTIONARY OR a FILE (D/F)? \n> ")
